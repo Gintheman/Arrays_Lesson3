8c37880d5b788ed3dbfe656e36a0287724692915
Eugene <joker3lolz@gmail.com>
2023-12-16 18:24:48 +0300

    Tried another solution to the 4th task.

--- 4th task/Program.cs
+++ 4th task/Program.cs
@@ -1,46 +1,110 @@
-﻿int natNum = new Random().Next(1, 100001);
-int num = 0;
-int amount = 0;
+﻿// int natNum = new Random().Next(1, 100001);
+// Console.Write(natNum);
+// int num = 0;
+// int amount = 0;
 
-// if(num >= 10000)
+// // if(num >= 10000)
+// // {
+// //     amount = 5;
+// // }
+// // else if(num >= 1000)
+// // {
+// //     amount = 4;
+// // }
+// // else if(num >= 100)
+// // {
+// //     amount = 3;
+// // }
+// // else if(num >= 10)
+// // {
+// //     num = 2;
+// // }
+// // else if(num >= 1)
+// // {
+// //     num = 1;
+// // }
+
+
+// while(natNum >= 1)
 // {
-//     amount = 5;
-// }
-// else if(num >= 1000)
-// {
-//     amount = 4;
-// }
-// else if(num >= 100)
-// {
-//     amount = 3;
-// }
-// else if(num >= 10)
-// {
-//     num = 2;
-// }
-// else if(num >= 1)
-// {
-//     num = 1;
+//     num = natNum / 10;
+//     amount++;
 // }
 
 
-while(natNum >= 1)
+
+
+
+// int[] array = new int[amount];
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     if (natNum >= 10000)
+//     {
+//         array[0] = natNum / 10000;
+//     }
+//     System.Console.Write(array[i] + " ");
+// };
+
+
+
+// с помощью интернета пришел к варианту ниже.
+
+
+static void Main()
 {
-    num = natNum / 10;
-    amount++;
+
+        // Вводим исходное число
+
+        Console.WriteLine("Enter natural number: ");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+        // Преобразуем число в строку
+
+        string numberString = num.ToString();
+
+
+
+        // Получаем количество цифр в числе
+
+        int length = numberString.Length;
+
+
+
+        // Создаем массив нужного размера
+
+        int[] array = new int[length];
+
+
+
+        // Заполняем массив цифрами числа
+
+        for (int i = 0; i < length; i++)
+
+        {
+
+            array[i] = int.Parse(numberString[i].ToString());
+
+        }
+
+
+
+        // Выводим массив на экран
+
+        Console.WriteLine("Массив цифр числа:");
+
+        foreach (int array in arrays)
+
+        {
+
+            Console.Write(array[i] + " ");
+
+        }
+
+
+
+        
 }
-
-
-
-
-
-int[] array = new int[amount];
-
-for(int i = 0; i < array.Length; i++)
-{
-    if (natNum >= 10000)
-    {
-        array[0] = natNum / 10000;
-    }
-    System.Console.Write(array[i] + " ");
-};
