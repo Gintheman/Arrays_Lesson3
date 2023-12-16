54330e83480e4e55bb74f8327294f33b20b3789f
Eugene <joker3lolz@gmail.com>
2023-12-16 16:34:49 +0300

    2nd task is done.

---
+++ 2nd task/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int [10];
+
+int count = 0;
+
+for( int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    System.Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Чётных чисел {count}");
