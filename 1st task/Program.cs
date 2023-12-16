717f8dda8dfcfbc56153ebc08841a66302472723
Eugene <joker3lolz@gmail.com>
2023-12-16 16:25:04 +0300

    First task is done.

--- 1st task/Program.cs
+++ 1st task/Program.cs
@@ -1,9 +1,20 @@
-﻿Console.WriteLine("");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿int[] array = new int[10];
 
-int[] array = new int[];
+int[] array2 = new int[new Random().Next(20, 91)];  
 
-for()
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
 {
+    array[i] = new Random().Next(1, 101);
+    System.Console.Write(array[i] + " ");
+
+    if (array[i] >= 20 && array[i] <= 90)
+    {
+        count++;
+    }
 
 };
+
+Console.WriteLine();
+Console.WriteLine(count);
