bf3823f289202e82d2055b5eea1da7f55ced9bbc
Eugene <joker3lolz@gmail.com>
2023-12-16 16:59:31 +0300

    3d task is failed. But i've tried my best.

---
+++ 3d task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пробовал решить в том числе с помощью интернета
+// Не представляю как это сделать.
+// Ни лекции, ни семинары, ни интернет не помогли.
+
+double[] array = new double[3];
+
+double max = 0;
+double min = array[0];
+
+double difference = 0;
+Random = new Random ();
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Random.NextDouble() * 100;
+    System.Console.Write(array[i] + " ");
+    if(array[i] > max)
+    {
+        array[i] = max;
+    };
+    if(array[i] < min)
+    {
+        array[i] = min;
+    };
+};
+
+
+
+Console.WriteLine();
+Console.WriteLine($"разница равна {difference = max - min}");
